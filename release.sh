30e1880cd81e92d468a220ec951c0b9896394cba
Victor Vrantchan <vrancean+github@gmail.com>
2016-05-23 20:17:51 -0400

    add static build tag to release script

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ mkdir -p build
 
 build() {
   echo -n "=> $1-$2: "
-  GOOS=$1 GOARCH=$2 go build -o build/$NAME-$1-$2 -ldflags "-X main.Version=$VERSION -X main.gitHash=`git rev-parse HEAD`" ./main.go
+  GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -o build/$NAME-$1-$2 -ldflags "-X main.Version=$VERSION -X main.gitHash=`git rev-parse HEAD`" ./main.go
   du -h build/$NAME-$1-$2
 }
 
